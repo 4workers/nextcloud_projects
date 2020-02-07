b736462d85c1916a4c0f414ad8f091e9f00872f2
Sergey Shliakhov <husband.sergey@gmail.com>
2020-02-07 13:57:26 +0100

    Extend a file list. Add extra class to project item
    Signed-off-by: Sergey Shliakhov <husband.sergey@gmail.com>

--- js/filelist_plugin.js
+++ js/filelist_plugin.js
@@ -19,10 +19,49 @@
 			'files'
 		],
 
-		attach: function (fileList) {
+        _extendFileList: function (fileList) {
+            // extend row prototype
+            var oldCreateRow = fileList._createRow;
+            fileList._createRow = function (fileData) {
+                var $tr = oldCreateRow.apply(this, arguments);
+                var isFavorite = false;
+                if (fileData.foreignId) {
+                    $tr.attr('data-foreign-id', fileData.foreignId);
+                    $tr.addClass('is-project');
+                }
+                return $tr;
+            };
+
+            var oldGetWebdavProperties = fileList._getWebdavProperties;
+            fileList._getWebdavProperties = function () {
+                var props = oldGetWebdavProperties.apply(this, arguments);
+                props.push(OC.Files.Client.PROPERTY_FOREIGN_ID);
+                return props;
+            };
+
+            fileList.filesClient.addFileInfoParser(function (response) {
+                var data = {};
+                var props = response.propStat[0].properties;
+                var foreignId = props[OC.Files.Client.PROPERTY_FOREIGN_ID];
+                if (foreignId) {
+                    foreignId = _.chain(foreignId).filter(function (xmlvalue) {
+                        return (xmlvalue.namespaceURI === OC.Files.Client.NS_OWNCLOUD && xmlvalue.nodeName.split(':')[1] === 'foreign-id');
+                    }).map(function (xmlvalue) {
+                        return xmlvalue.textContent || xmlvalue.text;
+                    }).value();
+                }
+                if (foreignId) {
+                    data.foreign_id = foreignId;
+                }
+                return data;
+            });
+        },
+
+        attach: function (fileList) {
 			if (this.allowedLists.indexOf(fileList.id) < 0) {
 				return;
 			}
+			this._extendFileList(fileList);
 		},
 	};
 })
