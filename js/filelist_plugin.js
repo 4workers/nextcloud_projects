d4d97f5eef4b6fc239c41c8dac16017c3aa739bf
Sergey Shliakhov <husband.sergey@gmail.com>
2020-02-10 07:42:10 +0100

    Lint
    Signed-off-by: Sergey Shliakhov <husband.sergey@gmail.com>

--- js/filelist_plugin.js
+++ js/filelist_plugin.js
@@ -19,45 +19,44 @@
 			'files'
 		],
 
-        _extendFileList: function (fileList) {
-            // extend row prototype
-            var oldCreateRow = fileList._createRow;
-            fileList._createRow = function (fileData) {
-                var $tr = oldCreateRow.apply(this, arguments);
-                var isFavorite = false;
-                if (fileData.foreignId) {
-                    $tr.attr('data-foreign-id', fileData.foreignId);
-                    $tr.addClass('is-project');
-                }
-                return $tr;
-            };
+		_extendFileList: function (fileList) {
+			// extend row prototype
+			var oldCreateRow = fileList._createRow;
+			fileList._createRow = function (fileData) {
+				var $tr = oldCreateRow.apply(this, arguments);
+				if (fileData.foreignId) {
+					$tr.attr('data-foreign-id', fileData.foreignId);
+					$tr.addClass('is-project');
+				}
+				return $tr;
+			};
 
-            var oldGetWebdavProperties = fileList._getWebdavProperties;
-            fileList._getWebdavProperties = function () {
-                var props = oldGetWebdavProperties.apply(this, arguments);
-                props.push(OC.Files.Client.PROPERTY_FOREIGN_ID);
-                return props;
-            };
+			var oldGetWebdavProperties = fileList._getWebdavProperties;
+			fileList._getWebdavProperties = function () {
+				var props = oldGetWebdavProperties.apply(this, arguments);
+				props.push(OC.Files.Client.PROPERTY_FOREIGN_ID);
+				return props;
+			};
 
-            fileList.filesClient.addFileInfoParser(function (response) {
-                var data = {};
-                var props = response.propStat[0].properties;
-                var foreignId = props[OC.Files.Client.PROPERTY_FOREIGN_ID];
-                if (foreignId) {
-                    data.foreignId = foreignId;
-                    data.icon = OCP.InitialState.loadState('projects', 'project-icon');
-                }
-                return data;
-            });
-        },
+			fileList.filesClient.addFileInfoParser(function (response) {
+				var data = {};
+				var props = response.propStat[0].properties;
+				var foreignId = props[OC.Files.Client.PROPERTY_FOREIGN_ID];
+				if (foreignId) {
+					data.foreignId = foreignId;
+					data.icon = OCP.InitialState.loadState('projects', 'project-icon');
+				}
+				return data;
+			});
+		},
 
-        attach: function (fileList) {
+		attach: function (fileList) {
 			if (this.allowedLists.indexOf(fileList.id) < 0) {
 				return;
 			}
 			this._extendFileList(fileList);
 		},
 	};
-})
-(OCA);
+})(OCA);
+
 OC.Plugins.register('OCA.Files.FileList', OCA.Files.ProjectsPlugin);
