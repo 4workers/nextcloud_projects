e90089ab7e21ca4d026d5333df1399d0493951ad
Sergey Shliakhov <husband.sergey@gmail.com>
2020-03-03 07:50:36 +0100

    Bootstrap acceptance tests
    Use jest instead of behat
    
    Signed-off-by: Sergey Shliakhov <husband.sergey@gmail.com>

---
+++ tests/acceptance/configuration.ts
@@ -0,0 +1,24 @@
+import { ConnectionOptions } from 'nextcloud-link';
+import { join }              from 'path';
+import * as fs               from 'fs';
+
+const dockerConfig = fs.readFileSync(join(__dirname, '../../docker-compose.yml')).toString();
+const overrideFile = join(__dirname, '../local.json');
+const overrideConfig = (fs.existsSync(overrideFile) ? JSON.parse(fs.readFileSync(overrideFile).toString()) : {});
+
+const baseConfig = {
+  address: 'localhost',
+  port: dockerConfig.match(/(\d+):80/)[1],
+  username: dockerConfig.match(/NEXTCLOUD_ADMIN_USER=(.*)/)[1],
+  password: dockerConfig.match(/NEXTCLOUD_ADMIN_PASSWORD=(.*)/)[1],
+  connectionOptions: null as ConnectionOptions
+};
+
+let configuration = Object.assign({}, baseConfig, overrideConfig);
+configuration.connectionOptions = {
+  username: configuration.username,
+  password: configuration.password,
+  url: `http://${configuration.address}:${configuration.port}`
+} as ConnectionOptions;
+
+export default configuration;
